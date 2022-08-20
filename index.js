6a89e7b08e2da05759c444a57cf9e8caa4627521
Yvan Nanfack <romyvan1999@gmail.com>
2022-08-20 22:42:05 +0100

    feat(app): home made redux complete

--- index.js
+++ index.js
@@ -1,9 +1,80 @@
-function todos (state = [], action) {
-  if ( action.type === 'ADD_TODO') {
-    return state.concat([action.todo])
-  }
+// Constants
+const ADD_TODO = 'ADD_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
 
-  return state
+// Utils functions
+function addTodoAction (todo) {
+  return {
+    type: ADD_TODO,
+    todo
+  }
+}
+
+function removeTodoAction(id) {
+  return {type: REMOVE_TODO,
+  id}
+}
+
+function toggleTodoAction(id) {
+  return {
+    type: TOGGLE_TODO,
+    id
+  }
+}
+
+function addGoalAction(goal) {
+  return {
+    type: ADD_GOAL,
+    goal
+  }
+}
+
+function removeGoalAction (id) {
+  return {
+    type: REMOVE_GOAL,
+    id
+  }
+}
+
+// Reducers
+function todos (state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return state.concat([action.todo])
+
+    case REMOVE_TODO: 
+      return state.filter(todo => todo.id !== action.id)
+
+    case TOGGLE_TODO:
+      return state.map((todo) => todo.id !== action.id ? todo : Object.assign({}, todo, {complete: !todo.complete}))
+
+    default: 
+      return state
+
+  }
+}
+
+function goals (state = [], action) {
+  switch (action.type) {
+    case ADD_GOAL: 
+      return state.concat([action.goal])
+
+    case REMOVE_GOAL: 
+      return state.filter(goal => goal.id !== action.id)
+
+    default: 
+      return state
+  }
+}
+
+function app (state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action)
+  }
 }
 
 // State manager
@@ -32,7 +103,7 @@ function createStore(reducer) {
   }
 }
 
-const store = createStore(todos)
+const store = createStore(app)
 
 store.subscribe(() => console.log('the new state is: ', store.getState()))
 
